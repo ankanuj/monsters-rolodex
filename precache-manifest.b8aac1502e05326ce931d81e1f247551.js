85b314b36dcf316c5f805294cf1be4dac4bc6c6d
ankanuj <s.singhanuj132@gmail.com>
2020-08-07 23:14:49 +0530

    Updates

---
+++ precache-manifest.b8aac1502e05326ce931d81e1f247551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c85d1cfb5a8f1e06f3620e3d690807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c062758e4c8fc49ea3b",
+    "url": "/static/css/main.8a32e57d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c062758e4c8fc49ea3b",
+    "url": "/static/js/main.e1d9538b.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
